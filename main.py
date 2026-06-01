26410d1fd2d546edb36312d509c2000218565d55
ArSi <silvan@arsija.net>
2026-06-01 09:14:08 +0200

    add troops

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 Fahrzeugtyp = input("Für welchen Fahrzeugtyp möchtest du die Supplies berechenen?:")
 Amount = input(f"Wieviele des Fahrzeugtyps {Fahrzeugtyp} brauchst du?:")
+Troops = input(f"Wieviel besatzung benötigt 1 {Fahrzeugtyp}?:")
